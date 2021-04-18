cc91f6cdb9709d86048c9044395b6b43442c4b55
J___ J___ <j-j12@o2.pl>
2021-04-18 21:23:38 +0200

    pole prostokata

---
+++ prostokat.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int a;
+int b;
+int wynik;
+    printf("wprowadz dlugosc boku a\n");
+    scanf("%d", &a);
+    printf("wprowadz dlugosc boku b\n");
+    scanf("%d", &b);
+    wynik = a*b;
+    printf("wynik=" "%d\n", wynik);
+
+    return 0;
+
+}
