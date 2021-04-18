b216f0c7b7b3b688358e0ba1a8a1f7c8f30448e1
Jakub Szczepankiewicz <szczepankiewiczjakub@gmail.com>
2021-04-18 21:54:20 +0200

    dodano opcje liczenia trojkata

--- prostokat.c
+++ prostokat.c
@@ -6,7 +6,18 @@ int main()
 
 int a;
 int b;
+int licznik;
+int podstawa;
+int wysokoœæ;
+ printf("wybierz funkcje \n");
+ printf("1. pole prostokata \n");
+ printf("2. pole trojkata \n");
+ scanf("%d", &licznik);
 int wynik;
+switch (licznik)
+{
+
+case 1:
     printf("wprowadz dlugosc boku a\n");
     scanf("%d", &a);
     printf("wprowadz dlugosc boku b\n");
@@ -14,6 +25,16 @@ int wynik;
     wynik = a*b;
     printf("wynik=" "%d\n", wynik);
 
-    return 0;
+    break;
+case 2:
+    printf("wprowadz dlugosc podstawy\n");
+    scanf("%d", &podstawa);
+    printf("wprowadz dlugosc wyskoœci\n");
+    scanf("%d", &wysokoœæ);
+    wynik = 0.5*podstawa*wysokoœæ;
+    printf("wynik=" "%d\n", wynik);
 
+    break;
+default:
+    printf("zly wybor\n");
 }
